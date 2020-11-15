27222283dd08abd586e0680b5ed79345596257dc
dasMulli <martin.andreas.ullrich@gmail.com>
2020-11-15 21:34:41 +0100

    Samples for DotNext Moscow 2020

---
+++ 10_CodeGen/CodeGenDemo/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CodeGenDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"{BuildVariables.Greeting} {BuildVariables.Name}!");
+            Console.ReadLine();
+        }
+    }
+}
